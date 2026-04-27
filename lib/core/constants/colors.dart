41f82686542d99a470a4c4d44826fe566765c9bf
NADDZ2510 <anhducstudy2510@gmail.com>
2026-04-27 21:27:06 +0700

    Them cac hang so mau

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Colors.black;
+  static const seconary = Colors.white;
+  static const textPrimary = Colors.white;
+  static const textSeconary = Colors.grey;
+
+}
